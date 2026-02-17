e3acce1eaf17708cab80c93fa883a28b0aba4564
StephenHody <stephenhody@gmail.com>
2026-02-16 23:39:42 -0500

    Thru Lesson 9
    Multiple players all in same game, controlled by each player and displaying sync'd

--- main.gd
+++ main.gd
@@ -1,9 +1,21 @@
 extends Node
 
+var player_scene: PackedScene = preload("uid://l4fulpkysuj5")
+
+@onready var multiplayer_spawner: MultiplayerSpawner = $MultiplayerSpawner
+
 
 func _ready():
+	multiplayer_spawner.spawn_function = func(data):
+		var player = player_scene.instantiate() as Player
+		player.name = str(data.peer_id)
+		player.input_multiplayer_authority = data.peer_id
+		return player
+
 	peer_ready.rpc_id(1)
+
 
 @rpc("any_peer", "call_local", "reliable")
 func peer_ready():
-	print("peer %s ready" % multiplayer.get_remote_sender_id())
+	var sender_id = multiplayer.get_remote_sender_id()
+	multiplayer_spawner.spawn({"peer_id": sender_id})
