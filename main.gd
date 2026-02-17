b8e104432ca0d244a95846eb4d6901702023e823
StephenHody <stephenhody@gmail.com>
2026-02-16 23:00:37 -0500

    Thru Lesson 6

---
+++ main.gd
@@ -0,0 +1,9 @@
+extends Node
+
+
+func _ready():
+	peer_ready.rpc_id(1)
+
+@rpc("any_peer", "call_local", "reliable")
+func peer_ready():
+	print("peer %s ready" % multiplayer.get_remote_sender_id())
