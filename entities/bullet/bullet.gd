38053d0b3e31ebeb32f5cbf4bb55eaee6b6546d4
StephenHody <stephenhody@gmail.com>
2026-02-18 23:09:11 -0500

    Thru Lesson 21

---
+++ entities/bullet/bullet.gd
@@ -0,0 +1,28 @@
+class_name Bullet
+extends Node2D
+
+const SPEED: int = 600
+@onready var life_timer: Timer = $LifeTimer
+
+var direction: Vector2
+
+
+func _ready() -> void:
+	life_timer.timeout.connect(_on_life_timer_timeout)
+
+
+func _process(delta: float):
+	global_position += direction * SPEED * delta
+
+
+func start(direction: Vector2):
+	self.direction = direction
+	rotation = direction.angle()
+
+
+func register_collision():
+	queue_free()
+
+func _on_life_timer_timeout():
+	if is_multiplayer_authority():
+		queue_free()
--- main.gd
+++ main.gd
@@ -3,6 +3,7 @@ extends Node
 var player_scene: PackedScene = preload("uid://l4fulpkysuj5")
 
 @onready var multiplayer_spawner: MultiplayerSpawner = $MultiplayerSpawner
+@onready var player_spawn_position: Marker2D = $PlayerSpawnPosition
 
 
 func _ready():
@@ -10,6 +11,7 @@ func _ready():
 		var player = player_scene.instantiate() as Player
 		player.name = str(data.peer_id)
 		player.input_multiplayer_authority = data.peer_id
+		player.global_position = player_spawn_position.global_position
 		return player
 
 	peer_ready.rpc_id(1)
